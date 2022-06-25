44ab3b75d650432a7fb2db60c5580f59c7d5af27
18646447 <Rainbow223344>
2022-06-25 20:48:43 +0300

    init commit

---
+++ src/main/java/test/example/recipes/FavouriteRecipesApplication.java
@@ -0,0 +1,13 @@
+package test.example.recipes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FavouriteRecipesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FavouriteRecipesApplication.class, args);
+    }
+
+}
